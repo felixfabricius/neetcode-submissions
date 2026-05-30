0f9eb8bdaaaba03e26fc36e15a72c786db695ca9
felixfabricius <62654597+felixfabricius@users.noreply.github.com>
2026-05-30 19:06:44 +0100

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        Possible solutions:
+        - Double for loop with complexity O(n^2)
+        - Iterating through from the back, keeping track of highest possible prices and max profit
+          The approach I've chosen here can also be implemented when looping forward. In that case,
+          we just need to keep track of the lowest buy price rather than the highest sell price.
+        """
+        best_sale_price = 1
+        max_profit = 0
+        for i in range(len(prices) - 1, 0, -1):
+            if prices[i] > best_sale_price:
+                best_sale_price = prices[i]
+            max_profit = max(max_profit, best_sale_price - prices[i - 1])
+        max_profit = max(max_profit, 0)
+        return max_profit
+            
