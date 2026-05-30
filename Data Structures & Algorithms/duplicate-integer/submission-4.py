17d0471597d28731f2f1df8b9c296ee0f54e2421
felixfabricius <62654597+felixfabricius@users.noreply.github.com>
2026-05-30 10:21:31 +0100

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        """
+        # O(n^2) solution
+        for i in range(0, len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+        
+        # This is also O(n) (because constructing the set is O(n))
+        # But this has worse constant factors than above approach, because
+        # we always construct the entire set
+        return len(set(nums)) != len(nums)
+        """
+        # O(n) solution
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        return False
