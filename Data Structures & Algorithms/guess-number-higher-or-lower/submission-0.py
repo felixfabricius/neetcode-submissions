97a50bd1885910a8a429c23abd7fe02872c479c3
felixfabricius <62654597+felixfabricius@users.noreply.github.com>
2026-06-05 15:00:34 +0100

    Add: guess-number-higher-or-lower - submission-0

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-0.py
@@ -0,0 +1,23 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l = 1
+        h = n
+
+        while l <= h:
+            m = l + (h - l) // 2
+            feedback = guess(m)
+            if feedback == 0:
+                return m
+            elif feedback == 1:
+                l = m + 1
+            else:
+                h = m - 1
+        
+        raise RuntimeError("Unreachable Code Reached")
