adddc6f97eb7040e8747f0a938b3f8aac4b21eac
felixfabricius <62654597+felixfabricius@users.noreply.github.com>
2026-06-13 13:01:02 +0100

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+Approach:
+- Will need to check each possible path and therefore each possible node.
+- Depth first search: O(n) time, O(h) space
+- Morris traversal: O(n) time, O(1) space
+"""
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
